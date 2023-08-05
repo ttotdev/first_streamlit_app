71860d51c8fbcbff71cbce228284402f77b90dd8
Tibor Tot <2491360+ttotdev@users.noreply.github.com>
2023-08-05 18:08:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,6 @@ sl.dataframe(fruits_to_show)
 
 # new section to display fruityvice API response
 import requests
+sl.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
 sl.text(fruityvice_response) 
