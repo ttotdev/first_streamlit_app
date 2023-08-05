500ace1c4d85592341e3b96e1afab080c7247e9d
Tibor Tot <2491360+ttotdev@users.noreply.github.com>
2023-08-05 10:42:47 -0400

    adding icons and a menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Bolied Fre-Range Egg")
+streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔Hard-Bolied Fre-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
