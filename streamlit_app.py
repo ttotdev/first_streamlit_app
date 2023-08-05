3fdfa63177890ba00e57801639911998dccca790
Tibor Tot <2491360+ttotdev@users.noreply.github.com>
2023-08-05 10:44:51 -0400

    expanding the menu page

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
-streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
-streamlit.text("🐔Hard-Bolied Fre-Range Egg")
-streamlit.text("🥑🍞 Avocado Toast")
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Bolied Fre-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
