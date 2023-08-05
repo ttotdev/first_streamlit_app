06c122ad13d82655b3264cd261b6b9900ad7cfb7
Tibor Tot <2491360+ttotdev@users.noreply.github.com>
2023-08-05 11:06:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,9 @@ sl.text('🥑🍞 Avocado Toast')
 
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+sl.multiselect('Pick some fruits:', list(fruit_list.index))
+
+
 sl.dataframe(my_fruit_list)
